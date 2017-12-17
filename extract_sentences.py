c97dcbc6e3f01a32efac36c7add5548befea145b
enik <eevanikkari@gmail.com>
2017-12-17 22:10:54 +0200

    fixed trie, created the extract sentences function that skips abbreviations

--- extract_sentences.py
+++ extract_sentences.py
@@ -1,42 +1,42 @@
 # -*- coding: utf-8 -*-
 import re
+from utils.trie import TrieJumper
 
 def read_wordlist_from_file(filename):
     with open(filename, 'r+') as f:
         read_data = f.read()
     return sorted(re.findall('[0-9]* *([^0-9]*)\n', read_data)) # return without counts in the beginning
 
-# TODO
-def test_if_abbreviation(string, abbreviations):
-    # TODO: add digits <d>
-    return -1
+def split_string_to_sentences(text, abbreviations):
+    # TODO : take into account digits <d>
+    # add abbreviations to the abbreviations checker
+    abb_checker = TrieJumper()
+    for abb in abbreviations:
+        abb_checker.add_word(abb)
 
-def split_string_to_sentences(string, abbreviations):
+    # loop trough the text and collect sentences
     sentences = []
-    prew_split_at = 0
-    for i in range(0,len(string)):
-        if(string[i] == '?' or string[i] == '!'):
-            sentences.append(string[prew_split_at:i+1])
-            prew_split_at = i+1
-        elif (string[i] == '.'):
-            # TODO : check abbreviations
-            sentences.append(string[prew_split_at:i + 1])
-            prew_split_at = i + 1
+    start_of_new_sentence = 0
+    i = 0
+    while i < len(text):
+        # break sentence at !, . and ? - take into account a string of them. F.ex !!!??
+        if text[i] in ['!', '.', '?']:
+            while i < len(text) and text[i] in ['!', '.', '?']:
+                i += 1
+            sentences.append(text[start_of_new_sentence: i])
+            start_of_new_sentence = i
+        # check if we are at the beginning of a abbreviation - if so, jump to it's end
+        i += abb_checker.make_jump(text[i:])
+        i += 1
+    # add last sentence to list (if it does not end in !?.)
+    if (i != len(text)):
+        sentences.append(text[start_of_new_sentence:])
     return sentences
 
-def split_string_to_sentences_with_default(string):
-    # TODO
-    print('abbreviations not yet supported')
-    #abbreviations_list_filename = "manual_abbreviations_list.txt"
-    #abbreviations_list = read_wordlist_from_file(abbreviations_list_filename)
-    return split_string_to_sentences(string, [])
 
-#abbreviations_list_filename = "manual_abbreviations_list.txt"
+abbreviations_list_filename = "manual_abbreviations_list.txt"
+abbreviations_list = read_wordlist_from_file(abbreviations_list_filename)
 
-#abbreviations_list = read_wordlist_from_file(abbreviations_list_filename)
-#print abbreviations_list
-
-#test_s = "Testilause. Onko tämä lause?Älä huuda!.. esim. tämä ei vielä voimi"
-#print (split_string_to_sentences(test_s, abbreviations_list))
-
-
+test_s = "Testilause. Onko tämä lause?Älä huuda!.. esim. tämä toimii jo. Mutta numerot kuten 123.123.123 eivät."
+sentences = split_string_to_sentences(test_s, abbreviations_list)
+print(sentences)
--- utils/trie.py
+++ utils/trie.py
@@ -1,5 +1,10 @@
 # Eeva Nikkari
 
+debug = False
+def dd(s: str):
+    if debug:
+        print(s)
+
 class TrieJumper:
 
     def __init__(self):
@@ -10,8 +15,10 @@ class TrieJumper:
         for c in word:
             next_leaf = in_leaf.child_with_value(c)
             if next_leaf is None:
+                dd('add child')
                 in_leaf = in_leaf.add_child(c)
             else:
+                dd('go to dext leaf')
                 in_leaf = next_leaf
         in_leaf.add_child('END')
 
@@ -20,35 +27,45 @@ class TrieJumper:
     def make_jump(self, leftower_of_text: str) -> int:
         in_leaf = self.root
         i = 0
-        while True:
+        while i < len(leftower_of_text):
             char_now = leftower_of_text[i]
             next_leaf = in_leaf.child_with_value(char_now)
             if(next_leaf is not None):
                 i+=1
                 in_leaf = next_leaf
+                dd(in_leaf == next_leaf)
             else:
                 break
 
         # check if we came to an end of a word
         if(in_leaf.child_with_value('END')):
+            dd("in leaf")
+            dd(in_leaf.value)
+            dd([c.value for c in in_leaf.children])
+            dd(in_leaf.children)
             return i
         else:
             return 0
 
 class Leaf():
-    def __init__(self, value, children=[]):
+    def __init__(self, value):
         self.value = value
-        self.children = children
+        self.children = list()
 
     def add_child(self, value):
         new_child = Leaf(value.lower())
         self.children.append(new_child)
+        dd('add child')
+        dd([c.value for c in self.children])
         #self.children = sorted(self.children, lambda child: child.value())
         return new_child
 
     def child_with_value(self, value):
         for child in self.children:
             if child.value == value.lower():
+                dd("return:" + str(child))
+                dd("child children")
+                dd(child.children)
                 return child
         return None
 
@@ -58,13 +75,21 @@ if __name__ == '__main__':
     segmenter.add_word('<d>.<d>.<d>')
 
     print(segmenter.make_jump("esim. kotona"))
+    print(segmenter.make_jump("esi kotona"))
     print(segmenter.make_jump("Esim. kotona"))
     print(segmenter.make_jump("<d>.<d>.<d> jälkeen"))
+    print(segmenter.make_jump("as <d>.<d>.<d> jälkeen"))
+    print(segmenter.make_jump("as <d>. jälkeen"))
 
-    # l1 = Leaf('a')
-    # l2 = l1.add_child('b')
-    # l22 = l1.add_child('bb')
-    # print(l1.value)
-    # print(l1.child_with_value('b'))
-    # print(l1.child_with_value('ads') is None)
-    # print(l1)
+    print(segmenter.make_jump("Testilause. jälkeen esim."))
+
+    l1 = Leaf('a')
+    l2 = l1.add_child('b')
+    l22 = l1.add_child('bb')
+    l23 = l22.add_child('ab')
+    print(l1.value)
+    print(l1.child_with_value('b'))
+    print(l1.child_with_value('ads') is None)
+    print([c.value for c in l1.children])
+    print([c.value for c in l22.children])
+    print(l23.value)
